7869cf4a0cb3df2832b54ab633e33e627affb9fc
SamZhang123 <31572111+SamZhang123@users.noreply.github.com>
2019-10-08 12:24:33 -0500

    Add files via upload

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,22 +1,35 @@
-package app;
+package interpreter;
+import java.util.Scanner;
+public class Main 
+{
 
-public class Main {
-
-  public static void main(String[] args) {
-    Lexicon lex = new Lexicon();
-
-    String newWord = lex.translate("food");
-    System.out.println(newWord);
-
-  }
+    public static void main(String[] args) 
+    {
+        Lexicon lex = new Lexicon();
+        System.out.println(decodeInput());
+    }
 
   /**
    * This method decodes a single word.
    * 
    * @param word The single word to be decoded.
    */
-  public static String decode(String word) {
-    return "";
+    public static String decode(String word) 
+    {
+    char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+    String output=word;
+    for(char v: vowels)
+    {
+        if(word.contains("ent") && word.charAt(word.indexOf("ent")-1)== v)
+        {
+            output=output.replaceFirst("ent","");
+        }
+        if(word.contains("ingy"))
+        {
+            output=output.replace("ingy","");
+        }
+    }
+    return output;
   }
 
   /**
@@ -24,23 +37,32 @@ public class Main {
    * 
    * @param message The sentence (multiple words) to be decoded.
    */
-  public static String decodeMessage(String message) {
-    return "";
-  }
+    public static String decodeMessage(String message) 
+    {
+        String output="";
+        int x=0;
+        for(int i=0; i<message.length()-1;i++)
+        {
+            if(message.charAt(i)==' ')
+            {
+                output+=decode(message.substring(x,i));
+                x=i;
+            }
+            
+        }
+        output+=decode(message.substring(x));
+        return output;
+    }
 
   /**
    * This method uses the decode(String) and a Scanner to decode a word specified
    * by the user.
    */
-  public static String decodeInput() {
-    return "";
-  }
-  
-  /**
-   * This method uses the Lexicon translate(String) and a Scanner to encode a word specified
-   * by the user.
-   */
-  public static String encodeInput() {
-    return "";
-  }
+    public static String decodeInput() 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter stuff in Lexicon:");
+        String stuff = input.nextLine();
+        return decodeMessage(stuff);
+    }
 }
